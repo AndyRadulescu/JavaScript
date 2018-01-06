8d4f9978d56dcfbbf304d798198dd8e7cdf497ca
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2018-01-06 17:15:27 +0200

    added the buget app

--- Dice-Game/app.js
+++ Dice-Game/app.js
@@ -79,17 +79,17 @@ function init() {
     activePlayer = 0;
 
     document.getElementById('score-0').textContent = 0;
-    document.getElementById('score-1').textContent = 0;
     document.getElementById('current-0').textContent = 0;
     document.getElementById('current-1').textContent = 0;
-
+    
     document.querySelector('#name-0').textContent = 'Player 1';
     document.querySelector('#name-1').textContent = 'Player 2';
-
+    
     document.querySelector('.dice').style.display = 'none';
-
+    
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
+    document.getElementById('score-1').textContent = 0;
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
 
